097ee73e7e9dcc49d747e979e9ba48c7d89f411c
Andrei Kamarouski <kamarouski.andrei@gmail.com>
2024-10-05 13:17:13 +0300

    test local ports

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
-	for i := 1; i <= 1024; i++ {
+	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
-			address := fmt.Sprintf("scanme.nmap.org:%d", i)
+			address := fmt.Sprintf("127.0.0.1:%d", i)
 
 			conn, err := net.Dial("tcp", address)
 			if err != nil {
