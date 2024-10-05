b42431a95ea35b0f1840b45ccc8f764104026e5f
Andrei Kamarouski <kamarouski.andrei@gmail.com>
2024-10-05 13:06:07 +0300

    reuse goroutime for scanning

--- main.go
+++ main.go
@@ -3,19 +3,26 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
 
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", i)
 
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			fmt.Printf("Port '%d' closed\n", i)
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port '%d' opened\n", i)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				fmt.Printf("Port '%d' closed\n", i)
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port '%d' opened\n", i)
+		}(i)
 	}
+	wg.Wait()
 }
