c8424bbb68a6b2de45a72d72e7e1a10300ff480b
Andrei Kamarouski <kamarouski.andrei@gmail.com>
2024-10-05 15:56:49 +0300

    organize worker pool

--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"sync"
 )
 
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for port := range ports {
+		fmt.Println(port)
+		wg.Done()
+	}
+}
+
 func main() {
-
+	ports := make(chan int, 100)
 	var wg sync.WaitGroup
-	for i := 1; i <= 65535; i++ {
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i < 1024; i++ {
 		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("127.0.0.1:%d", i)
-
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			conn.Close()
-			fmt.Printf("Port '%d' opened\n", i)
-		}(i)
+		ports <- i
 	}
 	wg.Wait()
+	close(ports)
 }
